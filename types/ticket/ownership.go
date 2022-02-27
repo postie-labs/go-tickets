97982ed8750b8ff2b391f5ee8e0b80420fb9d78d
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-02-28 07:29:55 +0900

    Extract OwnershipBody from Ownership

--- types/ticket/ownership.go
+++ types/ticket/ownership.go
@@ -1,6 +1,8 @@
 package ticket
 
 import (
+	"encoding/json"
+
 	crypto "github.com/postie-labs/go-crypto-lib"
 	"github.com/postie-labs/go-tickets/types"
 )
@@ -8,30 +10,67 @@ import (
 type OwnershipProtocolVersion uint16
 
 type Ownership struct {
-	// metadata
-	Hash            types.Hash
-	ProtocolVersion OwnershipProtocolVersion
-	Signatures      []Signature // signature of owners
+	Hash            types.Hash               `json:"hash"`
+	ProtocolVersion OwnershipProtocolVersion `json:"protocol_version"`
+	Signature       Signature                `json:"signature"`
 
-	// data
-	Timestamp  types.Timestamp
-	TicketHash types.Hash
-	Owners     []crypto.Addr
+	Body        OwnershipBody `json:"body"`
+	encodedBody []byte        `json:"-"`
 }
 
-func NewOwnership(protocolVersion OwnershipProtocolVersion, ticketHash types.Hash, owners []crypto.Addr) *Ownership {
+type OwnershipBody struct {
+	Timestamp  types.Timestamp `json:"timestamp"`
+	TicketHash types.Hash      `json:"ticket_hash"`
+	Owner      crypto.Addr     `json:"owner"`
+}
+
+func (ob *OwnershipBody) encode() ([]byte, error) {
+	return json.Marshal(ob)
+}
+
+func NewOwnership(protocolVersion OwnershipProtocolVersion, ticketHash types.Hash, owner crypto.Addr) (*Ownership, error) {
+	body := OwnershipBody{
+		Timestamp:  types.TimestampNow(),
+		TicketHash: ticketHash,
+		Owner:      owner,
+	}
+	encodedBody, err := body.encode()
+	if err != nil {
+		return nil, err
+	}
 	return &Ownership{
 		Hash:            types.Hash{},
 		ProtocolVersion: protocolVersion,
-		Signatures:      make([]Signature, 0),
+		Signature:       Signature{},
 
-		Timestamp:  types.TimestampNow(),
-		TicketHash: ticketHash,
-		Owners:     owners,
+		Body:        body,
+		encodedBody: encodedBody,
+	}, nil
+}
+
+func NewOwnershipFromBytes(data []byte) (*Ownership, error) {
+	ownership := Ownership{}
+	err := ownership.Decode(data)
+	if err != nil {
+		return nil, err
 	}
+	encodedBody, err := ownership.Body.encode()
+	if err != nil {
+		return nil, err
+	}
+	ownership.encodedBody = encodedBody
+	return &ownership, nil
 }
 
 // ops
+func (o *Ownership) Encode() ([]byte, error) {
+	return json.Marshal(o)
+}
+
+func (o *Ownership) Decode(data []byte) error {
+	return json.Unmarshal(data, o)
+}
+
 func (o *Ownership) GenerateHash() error {
 	return nil
 }
@@ -53,18 +92,18 @@ func (o *Ownership) GetProtocolVersion() OwnershipProtocolVersion {
 	return o.ProtocolVersion
 }
 
-func (o *Ownership) GetSignatures() []Signature {
-	return o.Signatures
+func (o *Ownership) GetSignature() Signature {
+	return o.Signature
 }
 
 func (o *Ownership) GetTimestamp() types.Timestamp {
-	return o.Timestamp
+	return o.Body.Timestamp
 }
 
 func (o *Ownership) GetTicketHash() types.Hash {
-	return o.TicketHash
+	return o.Body.TicketHash
 }
 
-func (o *Ownership) GetOwners() []crypto.Addr {
-	return o.Owners
+func (o *Ownership) GetOwner() crypto.Addr {
+	return o.Body.Owner
 }
