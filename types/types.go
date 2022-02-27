b5158c033ba0cf7fe64ed115d36e57baab4066ee
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-02-28 07:13:54 +0900

    Add Marshal, Unmarshal JSON for Hash struct

--- types/types.go
+++ types/types.go
@@ -1,12 +1,28 @@
 package types
 
-import "time"
+import (
+	"encoding/hex"
+	"time"
+)
 
 type (
 	Hash      [32]byte
 	Timestamp = time.Time
 	Data      []byte
 )
+
+func (hash *Hash) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + hex.EncodeToString(hash[:]) + `"`), nil
+}
+
+func (hash *Hash) UnmarshalJSON(data []byte) error {
+	tmp, err := hex.DecodeString(string(data[1 : len(data)-1]))
+	if err != nil {
+		return err
+	}
+	copy(hash[:], tmp)
+	return nil
+}
 
 func TimestampNow() Timestamp {
 	return time.Now().Round(0) // remove milliseconds
