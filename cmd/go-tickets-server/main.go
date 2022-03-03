7b546b26d9240ad5b103affd377235d0fd7c52b0
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-03-03 21:35:03 +0900

    Fill up main() with server init logic

--- cmd/go-tickets-server/main.go
+++ cmd/go-tickets-server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	pb "github.com/postie-labs/go-postie-proto/tickets"
+	tickets "github.com/postie-labs/go-tickets/app"
+	"github.com/postie-labs/go-tickets/server"
+	"google.golang.org/grpc"
+)
+
+const (
+	DefaultHost = "localhost"
+	DefaultPort = "7788"
+)
 
 func main() {
 	fmt.Println("things happen from here")
+
+	listener, err := net.Listen("tcp", net.JoinHostPort(DefaultHost, DefaultPort))
+	if err != nil {
+		panic(err)
+	}
+
+	// init app
+	app, err := tickets.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	// init server(handler)
+	billetterieServer := server.NewBilletterieServer(app)
+
+	// init grpcServer
+	opts := []grpc.ServerOption{} // TODO: add some options
+	grpcServer := grpc.NewServer(opts...)
+
+	// register server(handler) to grpcServer
+	pb.RegisterBilletterieServer(grpcServer, billetterieServer)
+
+	// start grpcServer
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
 }
