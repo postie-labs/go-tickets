8a2875d419a28eb3aa45beedaf268b1153eb06b5
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-05-11 07:20:48 +0900

    Add initClient() func

--- cli/root.go
+++ cli/root.go
@@ -2,9 +2,28 @@ package cli
 
 import (
 	"fmt"
+	"os"
+	"time"
 
+	cosmtypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/postie-labs/go-tickets/cli/tickets"
 	"github.com/spf13/cobra"
+	"github.com/terra-money/terra.go/client"
+	"github.com/terra-money/terra.go/key"
+)
+
+const (
+	DefaultLCDEndpoint            = "https://bombay-lcd.terra.dev"
+	DefaultChainID                = "bombay-12"
+	DefaultTimeout                = 10 // sec
+	DefaultContractAddressBench32 = "terra1al87aagg7asjyceu9x8f4xj554ddzlk9q2t8ls"
+)
+
+var (
+	LCDClient   *client.LCDClient
+	LCDEndpoint *string
+	ChainID     *string
+	Timeout     *int64
 )
 
 var RootCmd = &cobra.Command{
@@ -16,7 +35,13 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
+	cobra.OnInitialize(
+		InitClient,
+	)
 	cobra.EnableCommandSorting = false
+	LCDEndpoint = RootCmd.PersistentFlags().String("endpoint", DefaultLCDEndpoint, "LCDEndpoint")
+	ChainID = RootCmd.PersistentFlags().String("chain-id", DefaultChainID, "Chain ID")
+	Timeout = RootCmd.PersistentFlags().Int64("timeout", DefaultTimeout, "Timeout")
 	RootCmd.AddCommand(
 		tickets.IssueCmd,
 		tickets.TransferCmd,
@@ -24,4 +49,35 @@ func init() {
 		tickets.GetCmd,
 		tickets.QRCmd,
 	)
+}
+
+func InitClient() {
+	// derive privKey
+	mnemonic := os.Getenv("MNEMONIC")
+	if mnemonic == "" {
+		handleError(fmt.Errorf("failed to read MNEMONIC envrionment variable"))
+	}
+	privKeyBytes, err := key.DerivePrivKeyBz(mnemonic, key.CreateHDPath(0, 0))
+	if err != nil {
+		handleError(err)
+	}
+	privKey, err := key.PrivKeyGen(privKeyBytes)
+	if err != nil {
+		handleError(err)
+	}
+
+	// create LCDClient
+	LCDClient = client.NewLCDClient(
+		*LCDEndpoint,
+		*ChainID,
+		cosmtypes.NewDecCoinFromDec("uluna", cosmtypes.NewDecFromIntWithPrec(cosmtypes.NewInt(1133), 5)),
+		cosmtypes.NewDecFromIntWithPrec(cosmtypes.NewInt(150), 2),
+		privKey,
+		time.Duration(*Timeout)*time.Second,
+	)
+}
+
+func handleError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
--- cli/tickets/issue.go
+++ cli/tickets/issue.go
@@ -1,8 +1,14 @@
 package tickets
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
 var IssueCmd = &cobra.Command{
 	Use:   "issue",
 	Short: "issue a ticket",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// prepare
+		return nil
+	},
 }
