5d048997eb48a2d510af80af3d9ce1c384a21711
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-05-12 21:15:41 +0900

    Prepare GetCmd.RunE field

--- cli/tickets/get.go
+++ cli/tickets/get.go
@@ -5,4 +5,8 @@ import "github.com/spf13/cobra"
 var GetCmd = &cobra.Command{
 	Use:   "get",
 	Short: "get a ticket's information",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
 }
