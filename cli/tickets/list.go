2bf29c7ad0feb1570527deda5163dfff2bd677a3
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-05-14 11:52:22 +0900

    Fill up ListCmd.RunE field

--- cli/tickets/list.go
+++ cli/tickets/list.go
@@ -1,8 +1,52 @@
 package tickets
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	cosmtypes "github.com/cosmos/cosmos-sdk/types"
+	pb "github.com/postie-labs/proto/tickets"
+	"github.com/spf13/cobra"
+)
 
 var ListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list issued tickets",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ownerAddr := cosmtypes.AccAddress(LCDClient.PrivKey.PubKey().Address())
+		query := pb.QueryList{AllTokens: &pb.QueryList_List{Owner: ownerAddr.String()}}
+		queryBytes, err := json.Marshal(query)
+		if err != nil {
+			return err
+		}
+		queryStr := base64.StdEncoding.EncodeToString(queryBytes)
+		urlPath := fmt.Sprintf("%s/%s/%s/%s?query_msg=%s",
+			*LCDEndpoint,
+			"terra/wasm/v1beta1/contracts",
+			ContractAddr,
+			"store",
+			queryStr,
+		)
+		resp, err := http.Get(urlPath)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		var queryResp pb.QueryListResponse
+		err = json.Unmarshal(data, &queryResp)
+		if err != nil {
+			return err
+		}
+		for _, ticket := range queryResp.GetQueryResult().GetTokens() {
+			fmt.Println(ticket)
+		}
+		return nil
+	},
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/cosmos/cosmos-sdk v0.44.5
 	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/postie-labs/go-postie-lib v0.0.1
-	github.com/postie-labs/proto v0.0.7
+	github.com/postie-labs/proto v0.0.9
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
 	github.com/terra-money/terra.go v1.0.1-0.20211220063124-386f2075291e
